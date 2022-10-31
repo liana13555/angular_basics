c2fc82056e2d8d327714b9e3ef594e6db4860840
liana13555 <barishovaliana@gmail.com>
2022-10-31 10:52:05 +0100

    Added global styles

--- egghead-angular/src/app/app.component.ts
+++ egghead-angular/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <div class="container">    
     <h1>{{title}}</h1>
-    <app-habit-list></app-habit-list>    
+    <app-habit-list></app-habit-list> 
+  </div>  
   `,
-  styles: ['h1 {color: blue}']
+  styles: ['h1 {color: blue}'],
+
 })
 export class AppComponent {
   title = 'Habit Tracker';
